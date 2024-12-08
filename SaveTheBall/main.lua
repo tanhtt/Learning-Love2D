4c2942e834a07d707ba3bbc48665e1843d4c13f1
tanhtt <flywtan@gmail.com>
2024-12-08 21:57:00 +0700

    dont save the ball

--- SaveTheBall/main.lua
+++ SaveTheBall/main.lua
@@ -4,13 +4,16 @@ local enemy = require "Enemy"
 
 math.randomseed(os.time())
 
+-- Create Image
+
 local game = {
     difficulty = 1,
     state = {
         menu = true,
         running = false,
         paused = false,
-        ended = false
+        ended = false,
+        credit = false
     },
     points = 0,
     levels = {15, 30, 60, 120}
@@ -39,7 +42,8 @@ local player = {
 
 local buttons = {
     menu_state = {},
-    ended_state = {}
+    ended_state = {},
+    credit_state = {}
 }
 
 local enemies = {}
@@ -49,6 +53,7 @@ local function changeGameState(state)
     game.state["running"] = state == "running"
     game.state["paused"] = state == "paused"
     game.state["ended"] = state == "ended"
+    game.state["credit"] = state == "credit"
 end
 
 local function startNewGame()
@@ -71,6 +76,10 @@ function love.mousepressed(x, y, button, istouch, presses)
                 for index in pairs(buttons.ended_state) do
                     buttons.ended_state[index]:checkPressed(x,y,player.radius)
                 end
+            elseif game.state["credit"] then
+                for index in pairs(buttons.credit_state) do
+                    buttons.credit_state[index]:checkPressed(x,y,player.radius)
+                end
             end
         end
     end
@@ -80,13 +89,17 @@ function love.load()
     love.mouse.setVisible(false)
     love.window.setTitle("Save the ball!")
 
+    _G.avatar = love.graphics.newImage("sprites/white.png")
+
     buttons.menu_state.play_game = button("Play Game",startNewGame, nil, 120, 40)
-    buttons.menu_state.settings = button("Settings", nil, nil, 120, 40)
+    buttons.menu_state.credit = button("Credits", changeGameState, "credit", 120, 40)
     buttons.menu_state.exit_game = button("Exit", love.event.quit, nil, 120, 40)
 
     buttons.ended_state.replay_game = button("Replay", startNewGame, nil, 100, 50)
     buttons.ended_state.menu = button("Menu", changeGameState, "menu", 100, 50)
     buttons.ended_state.exit_game = button("Exit", love.event.quit, nil, 100, 50)
+
+    buttons.credit_state.menu = button("Menu", changeGameState, "menu", 100, 50)
 end
 
 function love.update(dt)
@@ -127,7 +140,7 @@ function love.draw()
 
     elseif game.state["menu"] then
         buttons.menu_state.play_game:draw(10, 20, 17, 10)
-        buttons.menu_state.settings:draw(10, 70, 17, 10)
+        buttons.menu_state.credit:draw(10, 70, 17, 10)
         buttons.menu_state.exit_game:draw(10, 120, 17, 10)
 
     elseif game.state["ended"] then
@@ -136,6 +149,16 @@ function love.draw()
         buttons.ended_state.exit_game:draw(love.graphics.getWidth() / 2.25, love.graphics.getHeight() / 1.33, 22, 10)
 
         love.graphics.printf(math.floor(game.points), fonts.massive.font, 0, love.graphics.getHeight() /2 - fonts.massive.size, love.graphics.getWidth(), "center")
+        
+    elseif game.state["credit"] then
+        buttons.credit_state.menu:draw(love.graphics.getWidth() - 200, love.graphics.getHeight() -100, 17, 10)
+        love.graphics.draw(avatar,100, 100, 0, 100)
+        love.graphics.printf("Made by Edd! Just following tutorial :)) <3", 
+            love.graphics.getWidth() * 1/3,  -- Tọa độ x
+            love.graphics.getHeight() * 2/3, -- Tọa độ y
+            love.graphics.getWidth() / 3,    -- Chiều rộng vùng văn bản
+            "center"                         -- Căn giữa
+        )
     end
 
     if not game.state["running"] then
